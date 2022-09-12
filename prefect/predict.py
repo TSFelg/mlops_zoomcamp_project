8634fc5120b1c84eaadec33cf7841a3c2293daa8
TSFelg <telmofelgueira@gmail.com>
2022-09-12 20:03:35 +0100

    update predict flow

--- prefect/predict.py
+++ prefect/predict.py
@@ -4,45 +4,72 @@ import pandas as pd
 import numpy as np
 from utils import *
 import mlflow
+from mlflow.tracking import MlflowClient
 from prefect import task, flow, get_run_logger
 from prefect.context import get_run_context
+from datetime import datetime
+from pathlib import Path
+from google.cloud import storage
 
 
 @task
 def get_data():
     url = "https://www.gstatic.com/covid19/mobility/Region_Mobility_Report_CSVs.zip"
-    extract_dir = "data"
+    extract_dir = "data/raw"
 
     zip_path, _ = urllib.request.urlretrieve(url)
     with zipfile.ZipFile(zip_path, "r") as f:
         f.extractall(extract_dir)
 
-    df = pd.read_csv("data/2022_PT_Region_Mobility_Report.csv")
+    df = pd.read_csv("data/raw/2022_PT_Region_Mobility_Report.csv")
+
+    return df
+
+
+@task
+def write_predictions(df):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket("project-predictions")  # your bucket name
+
+    data_path = Path("data")
+    output_path = Path(
+        data_path / "predictions" / datetime.today().strftime("%Y-%m-%d")
+    )
+    df.to_csv(output_path)
+
+    blob = bucket.blob(str(output_path))
+    blob.upload_from_filename(str(output_path))
 
     return df
 
 
 @flow
-def main(run_date=None):
+def main(run_date=None, log_stdout=True):
+    logger = get_run_logger()
+    logger.info("Hello World")
     df = get_data()
     df = clean_data(df)
 
     for i in np.arange(1, 7):
         last_day = df.iloc[-1:].index + pd.DateOffset(1)
-        df = df.append(pd.DataFrame(index=last_day))
+        df = pd.concat([df, pd.DataFrame(index=last_day)])
 
     df = feature_extraction(df)
     df = df.drop(columns=["y"])
 
     df = df.iloc[-2:]
 
-    RUN_ID = "52bf59cb151d46ac8827defb5ffa8363"
-    logged_model = f"gs://mlflow-mlops/4/{RUN_ID}/artifacts/model"
+    client = MlflowClient()
+    runs = client.search_runs(experiment_ids="5")
+    run = runs[0]
+    RUN_ID = run.info.run_id
+    logged_model = f"gs://mlflow-mlops/5/{RUN_ID}/artifacts/model"
     model = mlflow.pyfunc.load_model(logged_model)
 
-    pred = model.predict(df)
+    df["y"] = model.predict(df)
+    df["run_id"] = RUN_ID
 
-    print(pred)
+    df = write_predictions(df)
 
 
 if __name__ == "__main__":
